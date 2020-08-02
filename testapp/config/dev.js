b37324501b1657019a208c3cf9402d18412d252a
Firelord rupace <firelordgunner@gmail.com>
2020-08-02 21:40:24 +0545

    'testapp'

---
+++ testapp/config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID:
+    '807829796086-7bh1ni7fe6qk3s48ihs3jo0omq7b947b.apps.googleusercontent.com',
+  googleClientSecret: 'cAnBCAvkNT_wWxjOMKfcyK6a',
+  mongoURI: 'mongodb+srv://admin:arcadium@1@cluster0.hxo3c.mongodb.net/practice?retryWrites=true&w=majority',
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  },
+  cookieKey: '123123123',
+};
